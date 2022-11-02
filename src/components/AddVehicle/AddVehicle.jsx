cd413d6a936cd3cc0546db20a110fdea0276511d
Balu667 <balreddymahendra7@gmail.com>
2022-11-02 20:55:17 +0530

    almost done expect input validations

--- src/components/AddVehicle/AddVehicle.jsx
+++ src/components/AddVehicle/AddVehicle.jsx
@@ -1,95 +1,129 @@
-import React, { useState, useEffect } from 'react';
-import './AddVehicle.css';
-import { useNavigate } from 'react-router';
+import React, { useState, useEffect } from "react";
+import "./AddVehicle.css";
+import { useNavigate } from "react-router";
 
 export const AddVehicle = () => {
+  const navigate = useNavigate();
+  const [scenarios, setScenarios] = useState([]);
+  const [vehicleName, setVehicleName] = useState("");
+  const [speed, setSpeed] = useState("");
+  const [positionX, setPositionX] = useState("");
+  const [positionY, setPositionY] = useState("");
+  const [direction, setDirection] = useState("");
+  const [scenarioName, setScenarioName] = useState("");
 
-    const navigate = useNavigate();
-    const [scenarios, setScenarios] = useState([]);
-    const [vehicleName, setVehicleName] = useState("");
-    const [speed, setSpeed] = useState("");
-    const [positionX, setPositionX] = useState("");
-    const [positionY, setPositionY] = useState("");
-    const [direction, setDirection] = useState("");
-    const [scenarioName, setScenarioName] = useState("");
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("scenarios"));
+    setScenarios(data);
+  }, []);
 
-    useEffect(() => {
-        const data = JSON.parse(localStorage.getItem("scenarios"));
-        setScenarios(data);
-    }, []);
+  const submitHandler = () => {
+    scenarios.forEach((item, i) => {
+      if (item.scenarioName === scenarioName) {
+        const data = {
+          id: item.vehicles.length + 1,
+          vehicleName,
+          speed,
+          positionX,
+          positionY,
+          direction,
+          scenarioName,
+        };
+        if (positionX < 800 && positionX > 0) {
+          item.vehicles.push(data);
+          localStorage.setItem("scenarios", JSON.stringify(scenarios));
+          console.log(scenarios, "for");
+        }
+      }
+    });
+  };
 
-    const submitHandler = () => {
-        scenarios.forEach((item, i) => {
-            if (item.scenarioName === scenarioName) {
-                const data = {
-                    id: item.vehicles.length + 1,
-                    vehicleName,
-                    speed,
-                    positionX,
-                    positionY,
-                    direction,
-                    scenarioName
-                }
-                if(positionX < 800 && positionX > 0){
-                    item.vehicles.push(data);
-                    localStorage.setItem("scenarios", JSON.stringify(scenarios));
-                    console.log(scenarios, "for")
-                }
-                
-            }
-        })
-
-        console.log(scenarios[scenarioName], "data");
-    }
-    return (
-        <section className='add-vehicle-section'>
-            <div className='text'>Scenario / add</div>
-            <h3>Add Vehicle</h3>
-            <div className='add-vehicle-content'>
-                <div className='vehicle-container'>
-                    <div className='container-item'>
-                        <label>Scenario List</label>
-                        <select onChange={(e) => setScenarioName(e.target.value)} placeholder='Select Scenario'>
-                            <option value="none" selected disabled hidden>Select Scenario</option>
-                            {scenarios && scenarios.length > 0 && scenarios.map((item, i) => <option key={i + "p"}>{item.scenarioName}</option>)}
-                        </select>
-                    </div>
-                    <div className='container-item'>
-                        <label>Vehicle Name</label>
-                        <input onChange={(e) => setVehicleName(e.target.value)} value={vehicleName} type="text" placeholder='Target abc' />
-                    </div>
-                    <div className='container-item'>
-                        <label>Speed</label>
-                        <input onChange={(e) => setSpeed(e.target.value)} value={speed} type="number" />
-                    </div>
-                </div>
-                <div className='vehicle-container'>
-                    <div className='container-item'>
-                        <label>Position X</label>
-                        <input onChange={(e) => setPositionX(e.target.value)} value={positionX} type="number" />
-                        {(positionX < 0 || positionX > 800)   && <div className='error'>Position X should not be &#62; 800 and 	&#60; 0 </div> }
-                    </div>
-                    <div className='container-item'>
-                        <label>Position Y</label>
-                        <input onChange={(e) => setPositionY(e.target.value)} value={positionY} type="number" />
-                    </div>
-                    <div className='container-item'>
-                        <label>Direction</label>
-                        <select onChange={(e) => setDirection(e.target.value)}>
-                            <option value="null">Select Direction</option>
-                            <option value="towards">Towards</option>
-                            <option value="backwards">Backwards</option>
-                            <option value="upwards">Upwards</option>
-                            <option value="downwards">Downwards</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-            <div className='button-container'>
-                <button onClick={submitHandler}>Add</button>
-                <button>Reset</button>
-                <button onClick={() => navigate(-1)}>Go Back</button>
-            </div>
-        </section>
-    )
-}
+  const resetHandler = () => {
+    setPositionX("");
+    setPositionY("");
+    setVehicleName("");
+    setSpeed("");
+    setDirection("");
+  };
+  return (
+    <section className="add-vehicle-section">
+      <div className="text">Scenario / add</div>
+      <h3>Add Vehicle</h3>
+      <div className="add-vehicle-content">
+        <div className="vehicle-container">
+          <div className="container-item">
+            <label>Scenario List</label>
+            <select
+              onChange={(e) => setScenarioName(e.target.value)}
+              placeholder="Select Scenario"
+            >
+              <option value="none" selected disabled hidden>
+                Select Scenario
+              </option>
+              {scenarios &&
+                scenarios.length > 0 &&
+                scenarios.map((item, i) => (
+                  <option key={i + "p"}>{item.scenarioName}</option>
+                ))}
+            </select>
+          </div>
+          <div className="container-item">
+            <label>Vehicle Name</label>
+            <input
+              onChange={(e) => setVehicleName(e.target.value)}
+              value={vehicleName}
+              type="text"
+              placeholder="Target abc"
+            />
+          </div>
+          <div className="container-item">
+            <label>Speed</label>
+            <input
+              onChange={(e) => setSpeed(e.target.value)}
+              value={speed}
+              type="number"
+            />
+          </div>
+        </div>
+        <div className="vehicle-container">
+          <div className="container-item">
+            <label>Position X</label>
+            <input
+              onChange={(e) => setPositionX(e.target.value)}
+              value={positionX}
+              type="number"
+            />
+            {(positionX < 0 || positionX > 800) && (
+              <div className="error">
+                Position X should not be &#62; 800 and &#60; 0{" "}
+              </div>
+            )}
+          </div>
+          <div className="container-item">
+            <label>Position Y</label>
+            <input
+              onChange={(e) => setPositionY(e.target.value)}
+              value={positionY}
+              type="number"
+            />
+          </div>
+          <div className="container-item">
+            <label>Direction</label>
+            <select onChange={(e) => setDirection(e.target.value)}>
+              <option value="null">Select Direction</option>
+              <option value="Towards">Towards</option>
+              <option value="Backwards">Backwards</option>
+              <option value="Upwards">Upwards</option>
+              <option value="Downwards">Downwards</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={submitHandler}>Add</button>
+        <button onClick={resetHandler}>Reset</button>
+        <button onClick={() => navigate(-1)}>Go Back</button>
+      </div>
+    </section>
+  );
+};
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import './Home.css';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import { NavLink } from "react-router-dom";
 export const Home = () => {
   const [scenarios, setScenarios] = useState([]);
   const [scenarioName, setScenarioName] = useState("");
   const [vehicles, setVehicles] = useState([]);
 
-  const arr = [1, 2, 3, 4, 5, 6]
+  const arr = [1, 2, 3, 4, 5, 6];
 
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("scenarios"));
@@ -17,58 +17,87 @@ export const Home = () => {
     setScenarioName(e.target.value);
     scenarios.map((item, i) => {
       if (item.scenarioName === e.target.value) {
-        console.log(item.vehicles)
+        console.log(item.vehicles);
         setVehicles(item.vehicles);
       }
-    })
-  }
+    });
+  };
 
   const deleteVehicleById = (vehicle) => {
-    setVehicles(vehicles.filter(item => item.id !== vehicle.id));
+    setVehicles(vehicles.filter((item) => item.id !== vehicle.id));
     scenarios.map((item, i) => {
       if (item.scenarioName === scenarioName) {
-        scenarios[i].vehicles = vehicles.filter(item => item.id !== vehicle.id);
+        scenarios[i].vehicles = vehicles.filter(
+          (item) => item.id !== vehicle.id
+        );
         localStorage.setItem("scenarios", JSON.stringify(scenarios));
       }
-    })
-  }
+    });
+  };
 
   const generateSquareBoxs = (arr) => {
-    return <div className='horizontal'>{arr.map((item, i) => <div className='square'></div>)}</div>
-  }
+    return (
+      <div className="horizontal">
+        {arr.map((item, i) => (
+          <div className="square"></div>
+        ))}
+      </div>
+    );
+  };
 
   const simulationHandler = (e) => {
     const vehiclesDom = document.querySelectorAll(".vehicle");
 
     vehiclesDom.forEach((item) => {
-      const vehicle = vehicles.filter(vehicle => vehicle.id === +item.id)[0];
-      console.log(vehicle,"vwhicle");
-      console.log("dir",vehicle.direction);
-      switch(vehicle.direction){
-        case "Upwards": item.style.top = `${vehicle.positionY - vehicle.speed * 70}px`;
-        break;
-        case "Towards": item.style.left = `${vehicle.positionX + vehicle.speed * 70}px`;
-        break;
-        case "Downwards": item.style.top = `${vehicle.positionY - vehicle.speed * 70}px`;
-        break;
-        case "Towards": item.style.left = `${vehicle.positionX + vehicle.speed * 70}px`;
-        break;
-      }
-    })
-    
-    console.log(vehiclesDom,"scenarios");
+      const vehicle = vehicles.filter((vehicle) => vehicle.id === +item.id)[0];
+      console.log(vehicle, "vwhicle");
+      console.log("dir", vehicle.direction);
+      switch (vehicle.direction) {
+        case "Upwards":
+          item.style.top = `${
+            +vehicle.positionY - Number(vehicle.speed) * 70
+          }px`;
+          break;
+        case "Towards":
+          item.style.left = `${
+            +vehicle.positionX + Number(vehicle.speed) * 70
+          }px`;
+          break;
+        case "Downwards":
+          item.style.top = `${
+            +vehicle.positionY + Number(vehicle.speed) * 70
+          }px`;
 
-  }
+          break;
+        case "Backwards":
+          item.style.left = `${
+            +vehicle.positionX - Number(vehicle.speed) * 70
+          }px`;
+          break;
+      }
+    });
+  };
   return (
-    <section className='home-section'>
-      <div className='container-item'>
+    <section className="home-section">
+      <div className="container-item">
         <label>Scenario List</label>
-        <select onChange={(e) => selectScenarioHandler(e)} placeholder='Select Scenario'>
-          <option value="none" selected disabled hidden>Select Scenario</option>
-          {scenarios && scenarios.length > 0 && scenarios.map((item, i) => <option key={i + "s"} value={item.scenarioName}>{item.scenarioName}</option>)}
+        <select
+          onChange={(e) => selectScenarioHandler(e)}
+          placeholder="Select Scenario"
+        >
+          <option value="none" selected disabled hidden>
+            Select Scenario
+          </option>
+          {scenarios &&
+            scenarios.length > 0 &&
+            scenarios.map((item, i) => (
+              <option key={i + "s"} value={item.scenarioName}>
+                {item.scenarioName}
+              </option>
+            ))}
         </select>
       </div>
-      <div className='home-content'>
+      <div className="home-content">
         <table>
           <thead>
             <tr>
@@ -83,33 +112,59 @@ export const Home = () => {
             </tr>
           </thead>
           <tbody>
-            {vehicles && vehicles.length > 0 && vehicles.map((item, i) => {
-              return <tr key={item.id}><td>{item.id}</td>
-                <td>{item.vehicleName}</td>
-                <td>{item.positionX}</td>
-                <td>{item.positionY}</td>
-                <td>{item.speed}</td>
-                <td>{item.direction}</td>
-                <td><NavLink to="/addvehicle">Edit</NavLink></td>
-                <td><a onClick={() => deleteVehicleById(item)} href='#'>delete</a></td>
-              </tr>
-            })}
+            {vehicles &&
+              vehicles.length > 0 &&
+              vehicles.map((item, i) => {
+                return (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.vehicleName}</td>
+                    <td>{item.positionX}</td>
+                    <td>{item.positionY}</td>
+                    <td>{item.speed}</td>
+                    <td>{item.direction}</td>
+                    <td>
+                      <NavLink to="/addvehicle">Edit</NavLink>
+                    </td>
+                    <td>
+                      <a onClick={() => deleteVehicleById(item)} href="#">
+                        delete
+                      </a>
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>
-      <div className='home-buttons'>
+      <div className="home-buttons">
         <button onClick={simulationHandler}>Start Simulation</button>
         <button>Stop Simulation</button>
       </div>
-      <div className='graph-container'>
-        <div className='graph'>
-          {arr.map((item) => generateSquareBoxs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]))}
+      <div className="graph-container">
+        <div className="graph">
+          {arr.map((item) =>
+            generateSquareBoxs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+          )}
           {vehicles.map((item, i) => {
-          console.log(item)
-          return <div style={{position:"absolute",top:`${item.positionY}px`,left:`${item.positionX}px`}} className='vehicle' id={item.id} key={item.id}>{item.id}</div>
-        })}
+            console.log(item);
+            return (
+              <div
+                style={{
+                  position: "absolute",
+                  top: `${item.positionY}px`,
+                  left: `${item.positionX}px`,
+                }}
+                className="vehicle"
+                id={item.id}
+                key={item.id}
+              >
+                {item.id}
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
